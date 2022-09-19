f7f70381ee668efa3a9fa9665e58ce170bfdbba0
Pranay Kothari <pranay.kothari109@gmail.com>
2022-09-19 16:06:39 +0530

    updated schema for products

--- models/productModel.js
+++ models/productModel.js
@@ -3,6 +3,13 @@ const { Schema, model } = require("mongoose");
 const schema = new Schema({
     product_name: { type: 'string', required: true },
     category: { type: 'string', required: true },
+    price: Number,
+    description: String,
+    image: String,
+    rating: { 
+        rate: Number,
+        count: Number
+    }
 });
 
 module.exports = model('products', schema);
