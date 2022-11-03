ef792e510544f1779ad2c54f0a01b711c0aede71
Pranay Kothari <pranay.kothari109@gmail.com>
2022-11-03 20:37:09 +0530

    updated user schema: added address field

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,10 +1,17 @@
 const { Schema, model } = require("mongoose");
 
+const addressSchema = new Schema({
+    name: { type: 'string', required: true},
+    address: { type: 'string', required: true},
+    pincode: { type: 'number', required: true}
+})
+
 const schema = new Schema({
     name: { type: 'string', required: true },
     email: { type: 'string', required: true },
     password: { type: 'string', required: true},
-    role: {type: 'string', default:'user'}
+    role: {type: 'string', default:'user'},
+    address: addressSchema
 });
 
 module.exports = model('users', schema);
